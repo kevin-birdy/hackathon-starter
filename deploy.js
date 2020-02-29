a515b7600a6b1ce4dc3f952627cfedf157563fb9
Michael Riddering <riddering@Michaels-MacBook-Pro.local>
2020-02-28 18:07:57 -0700

    testing

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.95.227.123',
+      host: '3.94.179.57',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
